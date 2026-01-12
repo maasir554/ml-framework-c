4e1548bdbf69e0e0c5e28884e3b9ad4305a7ef22
maasir554 <maasir554@gmail.com>
2026-01-12 22:07:29 +0530

    add comments in arena header

--- arena.h
+++ arena.h
@@ -3,6 +3,7 @@
 #include<string.h>
 #include<stdbool.h>
 
+// short form for commonly used types
 typedef int8_t i8;
 typedef int16_t i16;
 typedef int32_t i32;
@@ -15,18 +16,26 @@ typedef uint64_t u64;
 typedef i8 b8;
 typedef i32 b32;
 
+// the actual arena structure
 typedef struct{ 
     u64 reserve_size, commit_size, pos, commit_pos;
 } mem_arena;
 
+// type agnostic minimum and maximum
+#define MIN(a, b) (((a)<(b)) ? (a) : (b))
+#define MAX(a, b) (((a)>(b)) ? (a) : (b))
+
+// get how many Bytes in the bigger units like KiB, MiB, GiB
 #define KiB(n) ((u64)(n) << 10)
 #define MiB(n) ((u64)(n) << 20)
 #define GiB(n) ((u64)(n) << 30)
 #define ALIGN_UP_POWER_2(n, p) (((u64)(n) + (u64)(p) - 1)&(~((u64)(p)-1)))
 
+// Power aligned to powers of two for faster access by CPU
 #define ARENA_BASE_POS (sizeof(mem_arena))
 #define ARENA_ALIGN (sizeof(void*))
 
+// Actual Arena memory allocating and de-allicating funcions 
 mem_arena* arena_create(u64 reserve_size, u64 commit_size);
 void arena_destroy(mem_arena* arena);
 void* arena_push(mem_arena* arena, u64 size, b32 non_zero);
@@ -34,16 +43,15 @@ void arena_pop(mem_arena* arena, u64 size);
 void arena_pop_to(mem_arena* arena, u64 pos);
 void arena_clear(mem_arena* arena);
 
+// helper functions that interface Operating System.
 u32 plat_get_pagesize(void);
 void* plat_mem_reserve(u64 size);
 b32 plat_mem_commit(void* ptr, u64 size);
 b32 plat_mem_decommit(void* ptr, u64 size);
 b32 plat_mem_release(void* ptr, u64 size);
 
+// Utility macros to be used in applications.
 #define PUSH_STRUCT(arena, T) (T*)arena_push((arena), sizeof(T), false)
 #define PUSH_STRUCT_NZ(arena, T) (T*)arena_push((arena), sizeof(T), true)
 #define PUSH_ARRAY(arena, T, n) (T*)arena_push((arena), sizeof(T)*(n), false)
 #define PUSH_ARRAY_NZ(arena, T, n) (T*)arena_push((arena), sizeof(T)*(n), true)
-
-#define MIN(a, b) (((a)<(b)) ? (a) : (b))
-#define MAX(a, b) (((a)>(b)) ? (a) : (b))
